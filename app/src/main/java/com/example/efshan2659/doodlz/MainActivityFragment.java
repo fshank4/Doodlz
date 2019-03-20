5eba05bdcc4bff8519adfb9c4d6ec674d8f16c0e
Eliza <fshank4@comcast.net>
2019-03-20 13:15:59 -0400

    Add MainActivityFragment.java Method OnCreateView

--- app/src/main/java/com/example/efshan2659/doodlz/MainActivityFragment.java
+++ app/src/main/java/com/example/efshan2659/doodlz/MainActivityFragment.java
@@ -1,22 +1,56 @@
+// MainActivityFragment.java
+// Fragment in which the DoodleView is displayed
 package com.example.efshan2659.doodlz;
 
-import android.support.v4.app.Fragment;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
+    private DoodleView doodleView; // handles touch events and draws
+    private float acceleration;
+    private float currentAcceleration;
+    private float lastAcceleration;
+    private boolean dialogOnScreen = false;
 
-    public MainActivityFragment() {
-    }
+    // value used to determine whether the user shook the device to erase
+    private static final int ACCELERATION_THRESHOLD = 100000;
 
+    // used to identify the request for using external storage, which
+    // the save image feature needs
+    private static final int SAVE_IMAGE_PERMISSION_REQUEST_CODE = 1;
+
+    // called when Fragment's view needs to be created
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view =
+                inflater.inflate(R.layout.fragment_main, container, false);
+
+        setHasOptionsMenu(true); // this fragment has menu items to display
+
+        // get reference to the DoodleView
+        doodleView = (DoodleView) view.findViewById(R.id.doodleView);
+
+        // initialize acceleration values
+        acceleration = 0.00f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+        return view;
     }
 }
